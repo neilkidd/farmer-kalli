7c966e26f580a3e897b53b6e3d1cb49ba9e77836
Neil Kidd <neil.a.kidd@gmail.com>
2020-07-08 14:49:31 +0100

    Test zero bags is 0

--- src/Calculator.test.js
+++ src/Calculator.test.js
@@ -1,8 +1,18 @@
 test('One bag cost £0.25', () => {
-    const cost = CalculateCost(1);
+    const cost = calculateCost(1);
+    
     expect(cost).toBe(0.25);
 });
 
-function CalculateCost(bag){
+test('Zero bags is £0.00', () => {
+    const cost = calculateCost(0);
+
+    expect(cost).toBe(0.00);
+});
+
+function calculateCost(bags){
+    if(bags === 0){
+        return 0;
+    }
    return 0.25;
 }

8aa60de36780f0d3016d55e2cb11f5df64fc061d
Neil Kidd <neil.a.kidd@gmail.com>
2020-07-08 15:02:20 +0100

    Calculates 3 bags cost

--- src/Calculator.test.js
+++ src/Calculator.test.js
@@ -16,12 +16,21 @@ test('Two bags cost 0.75', () => {
     expect(cost).toBe(0.75);
 });
 
+test('Three bags cost 1.25', () => {
+    const cost = calculateCost(3);
+
+    expect(cost).toBe(1.25);
+});
+
 function calculateCost(bags){
     if(bags === 0){
         return 0;
     }
     else if(bags === 2){
         return 0.75;
+    }
+    else if(bags === 3){
+        return 1.25;
     }
    return 0.25;
 }

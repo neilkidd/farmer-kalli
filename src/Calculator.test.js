7333afc7e4a4a568022210b45bc3804ce21516f5
Neil Kidd <neil.a.kidd@gmail.com>
2020-07-08 14:55:12 +0100

    Test 2 bags is 50p

--- src/Calculator.test.js
+++ src/Calculator.test.js
@@ -1,18 +1,27 @@
-test('One bag cost £0.25', () => {
-    const cost = calculateCost(1);
-    
-    expect(cost).toBe(0.25);
-});
-
-test('Zero bags is £0.00', () => {
+test('Zero bags is 0.00', () => {
     const cost = calculateCost(0);
 
     expect(cost).toBe(0.00);
 });
 
+test('One bag cost 0.25', () => {
+    const cost = calculateCost(1);
+
+    expect(cost).toBe(0.25);
+});
+
+test('Two bags cost 0.50', () => {
+    const cost = calculateCost(2);
+
+    expect(cost).toBe(0.50);
+});
+
 function calculateCost(bags){
     if(bags === 0){
         return 0;
+    }
+    else if(bags === 2){
+        return 0.50;
     }
    return 0.25;
 }

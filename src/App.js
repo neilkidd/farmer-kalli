1f67fb947f120f47d14097ba80466d50848c966a
Neil Kidd <neil.a.kidd@gmail.com>
2020-07-08 11:46:22 +0100

    First basic calculator working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
+function App(props) {
+
+  const [totalCost, setTotalCost] = useState(0);
+
+  const handleSubmit = (event) =>{
+    event.preventDefault();
+  };
+
+  const Calculate = (bags) =>{
+    setTotalCost(bags * 25);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" onSubmit={handleSubmit}>
+      <form name='calculator'>
+       Bags: <input type='number' min="0" max="1000" name='bags' value={props.bags} onChange={(e) => Calculate(e.target.value)}/>
+        
+        
+      </form>
+      <div>
+        Total Cost: {totalCost}
+      </div>
     </div>
   );
 }

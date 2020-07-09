98ee37e69a0435b6c41e0bb72c51bca78c5616ea
Neil Kidd <neil.a.kidd@gmail.com>
2020-07-09 11:22:10 +0100

    Prettify

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders number of bags', () => {
+test('renders number of bags of corn', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Number of bags/i);
+  const linkElement = getByText(/Bags of Corn:/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Calculator = (props) => {
     return <form name='calculator'>
-        Number of Geese: <input type='number' min="0" max="1000" name="geese" value={props.numberOfGeese} onChange={(e) => props.setNumberOfGeese(e.target.value)}/>
-        Number of bags: <input type='number' min="0" max="1000" name="sacks" value={props.sacksOfCorn} onChange={(e) => props.setSacksOfCorn(e.target.value)}/>
+        Number of Geese: <input type='number' min="0" max="1000" name="geese" value={props.numberOfGeese} onChange={(e) => props.setNumberOfGeese(e.target.value)}/><br/>
+        Bags of Corn: <input type='number' min="0" max="1000" name="sacks" value={props.sacksOfCorn} onChange={(e) => props.setSacksOfCorn(e.target.value)}/><br/>
         <button  onClick={props.handleCalculate}>Calculate</button>
    </form>;
 }
